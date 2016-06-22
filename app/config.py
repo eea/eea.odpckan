73714b96b7d8f83926b18284f6af0a99e924cdec
Dragos Chirila <dragos.chirila@eaudeweb.ro>
2016-06-22 18:19:33 +0300

    Fixed missing section options.
    refs #72772

--- app/config.py
+++ app/config.py
@@ -18,7 +18,6 @@ logger.addHandler(ch)
 parser = SafeConfigParser()
 parser.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), '.secret'))
 
-#preload some defaults to handle missing sections/options
 try:
     SECRET_RABBITMQ_HOST = parser.get('RABBITMQ', 'HOST')
 except:
@@ -27,11 +26,6 @@ try:
     SECRET_RABBITMQ_PORT = parser.get('RABBITMQ', 'PORT')
 except:
     SECRET_RABBITMQ_PORT = '5672'
-try:
-    SECRET_CKAN_ADDRESS = parser.get('CKAN', 'ADDRESS')
-except:
-    SECRET_CKAN_ADDRESS = 'missing'
-
 rabbit_config = {
     'rabbit_host': os.environ.get('RABBITMQ_HOST', SECRET_RABBITMQ_HOST),
     'rabbit_port': int(os.environ.get('RABBITMQ_PORT', SECRET_RABBITMQ_PORT)),
@@ -39,15 +33,31 @@ rabbit_config = {
     'rabbit_password': parser.get('RABBITMQ', 'PASSWORD')
 }
 
+try:
+    SECRET_CKAN_ADDRESS = parser.get('CKAN', 'ADDRESS')
+except:
+    SECRET_CKAN_ADDRESS = 'missing'
 ckan_config = {
     'ckan_address': os.environ.get('CKAN_ADDRESS', SECRET_CKAN_ADDRESS),
     'ckan_apikey': parser.get('CKAN', 'APIKEY')
 }
 
+try:
+    SECRET_SERVICES_EEA = parser.get('SERVICES', 'EEA')
+except:
+    SECRET_SERVICES_EEA = 'missing'
+try:
+    SECRET_SERVICES_SDS = parser.get('SERVICES', 'SDS')
+except:
+    SECRET_SERVICES_SDS = 'missing'
+try:
+    SECRET_SERVICES_ODP = parser.get('SERVICES', 'ODP')
+except:
+    SECRET_SERVICES_ODP = 'missing'
 services_config = {
-    'eea': os.environ.get('SERVICES_EEA', parser.get('SERVICES', 'EEA')),
-    'sds': os.environ.get('SERVICES_SDS', parser.get('SERVICES', 'SDS')),
-    'odp': os.environ.get('SERVICES_ODP', parser.get('SERVICES', 'ODP'))
+    'eea': os.environ.get('SERVICES_EEA', SECRET_SERVICES_EEA),
+    'sds': os.environ.get('SERVICES_SDS', SECRET_SERVICES_SDS),
+    'odp': os.environ.get('SERVICES_ODP', SECRET_SERVICES_ODP)
 }
 
 other_config = {
